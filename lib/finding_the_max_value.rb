45080d523f84be871a3ffcbe140fd82e8e11336e
Kat Leight <leightkt@gmail.com>
2020-11-20 22:59:17 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,20 @@
+# def find_max_value(array)
+#   # Add your solution here
+#   max_value = 0
+#   array.length.times do |index|
+#     if array[index] > max_value
+#       max_value = array[index]
+#     end
+#   end
+#   return max_value
+# end
+
 def find_max_value(array)
-  # Add your solution here
   max_value = 0
-  array.length.times do |index|
-    if array[index] > max_value
-      max_value = array[index]
+  array.each{|i|
+    if i > max_value
+      max_value = i
     end
-  end
+  }
   return max_value
 end
